f0f46e12f0c77c263523b958e634aec752fc78e7
Jonathan Ramos <jhonyramos46@gmail.com>
2023-02-28 00:04:08 -0300

    Execicio if/else 05

--- 01 - fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-01.js
+++ 01 - fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-01.js
@@ -25,18 +25,18 @@ else {
 
 //if/else 02
 
-let num1 = 7;
-let num2 = 7;
-let num3 = 7;
+let number1 = 7;
+let number2 = 7;
+let number3 = 7;
 
-if (num1 > num2 && num1 > num3 ) {
-  console.log( num1 + ' Higher number');
+if (number1 > number2 && number1 > number3 ) {
+  console.log( number1 + ' Higher number');
 }
-else if ( num2 > num1 && num2 > num3){
-  console.log(num2 + ' Higher number');
+else if ( number2 > number1 && number2 > number3){
+  console.log(number2 + ' Higher number');
 }
-else if ( num3 > num1 && num3 > num2){
-  console.log(num3 + ' Higher number');
+else if (number3 > number1 && number3 > number2){
+  console.log(number3 + ' Higher number');
 }
 else {
   console.log('Invalid information');
@@ -57,7 +57,7 @@ else {
   console.log('Zero');
 }
 
-//if/else
+//if/else 04
 
 let angle1 = 60;
 let angle2 = 60;
@@ -89,10 +89,36 @@ switch (peça) {
   case 'rainha':
     console.log(' Ela pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente');
     break;
-    case 'rei':
+  case 'rei':
       console.log('O rei pode se mover para qualquer casa adjacente. Assim, ele pode mover-se uma casa em qualquer direção');
       break;
   default:
     console.log('informação invalida');
     break;
+}
+
+// if/else 05
+
+let porcentagem = -1;
+
+if ( porcentagem >= 90){
+  console.log('A');
+}
+else if ( porcentagem >= 80){
+  console.log('B');
+}
+else if ( porcentagem >= 70){
+  console.log('C');
+}
+else if ( porcentagem >= 60){
+  console.log('D');
+}
+else if ( porcentagem >= 50){
+  console.log('E');
+}
+else if ( porcentagem > 0 && porcentagem < 50){
+  console.log('F');
+}
+else if (porcentagem > 100 || porcentagem < 0){
+  console.log('Informação invalida');
 }
