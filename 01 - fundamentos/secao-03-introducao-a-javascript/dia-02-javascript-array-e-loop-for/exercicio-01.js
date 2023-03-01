af7bec2921901917c1b8853c18e3a07c15c3cdc0
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-01 09:12:23 -0300

    exercicio-01.05 maior numero

--- 01 - fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
+++ 01 - fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio-01.js
@@ -2,7 +2,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let index = 0; index < numbers.length; index += 1 ){
-    console.log(numbers[index]);
-    soma += numbers[index]
+    soma += numbers[index] /10
 }
 console.log(soma);
+ if (soma > 20){
+    console.log('Valor maior que 20');
+ }
+ else {
+    console.log('Valor menor ou igual a 20');
+ }
