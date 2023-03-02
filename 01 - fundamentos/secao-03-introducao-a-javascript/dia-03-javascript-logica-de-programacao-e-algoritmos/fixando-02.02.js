ad053d9b5da5601e06b9c3670f097fa813eef615
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-02 12:43:23 -0300

    fixando-02.03

---
+++ 01 - fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos/fixando-02.02.js
@@ -0,0 +1,22 @@
+let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+
+  function removeClient (client){
+    
+    if(typeof client === 'string'){
+      let customerFound = false;
+      for (index = 0; index < clientesTrybeBank.length; index += 1){
+        if(client === clientesTrybeBank[index]){
+        clientesTrybeBank.splice(index, 1)
+        customerFound= true;
+        return 'Client remove sucessfully'
+      }
+    }
+  if(customerFound === false){
+    console.log('customer not found ');
+    }
+  } else{
+    console.log('O parâmetro passado deve ser do tipo "string"');
+  }
+}
+removeClient(8)
+console.log(clientesTrybeBank);
