4919ad1e97bd7f13386de9b772fccae605953262
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-03 01:25:47 -0300

    exercicio-02 object

--- 01 - fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/texteObjeto.js
+++ 01 - fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/texteObjeto.js
@@ -1,25 +1,52 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
+let order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
 };
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim',
-};
-
-for (let key in info) {
-  if (
-    key === 'recorrente' &&
-    info[key] === 'Sim' &&
-    info2[key] === 'Sim'
-  ) {
-    console.log('Ambos recorrentes');
-  } else {
-    console.log(info[key] + ' e ' + info2[key]);
-  }
+function customerInfo(order) {
+  // Adicione abaixo as informações necessárias.
 }
+
+customerInfo(order);
+
+function orderModifier(order) {
+  let newBuyer = order.name = 'Luiz Silva';
+  let newTotal = order.payment.total = '50';
+  let pizzas = Object.keys(order.order.pizza);
+  let drinks = order.order.drinks.coke.type;
+
+  console.log('Olá ' + newBuyer + ', o total do seu pedido de ' + pizzas[0] + ', ' + pizzas[1] + ' e ' + drinks + ' é R$ ' + newTotal + ',00.'); // Exiba a mensagem desejada
+}
+
+orderModifier(order);
