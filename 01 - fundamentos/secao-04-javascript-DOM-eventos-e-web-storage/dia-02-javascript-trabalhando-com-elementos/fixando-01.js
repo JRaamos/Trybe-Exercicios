5af52f2a2630297ee4ab4258d73089c18b8085cb
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-09 16:20:27 -0300

    fixando 1

---
+++ 01 - fundamentos/secao-04-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/fixando-01.js
@@ -0,0 +1,15 @@
+let element = document.getElementById('elementoOndeVoceEsta');
+let element2 = element.parentNode
+
+element2.style.background = 'rgb(76, 164, 109)'
+
+let element3 = document.getElementById('primeiroFilhoDoFilho').innerText = 'adicionei um texto top kkkkk'
+
+let element4 = element2.firstElementChild
+
+let element5 = element4.nextElementSibling
+
+let element6 = element5.nextSibling
+
+let element7 = element5.nextElementSibling
+console.log(element7);
