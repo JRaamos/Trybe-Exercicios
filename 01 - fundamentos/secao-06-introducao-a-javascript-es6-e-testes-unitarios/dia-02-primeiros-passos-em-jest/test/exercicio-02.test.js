ac55913a1e44e20b63207cb92d9987f387a18d45
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-28 19:15:39 -0300

    exercicio-03

--- 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/test/exercicio-02.test.js
+++ 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/test/exercicio-02.test.js
@@ -9,11 +9,11 @@ describe('A função myFizzBuzz(num) recebe um número num como parâmetro', () 
   })
   it('Execute a função myFizzBuzz(num), sendo num um número divisível por 5, e verifique se o retorno é o esperado.', () => {
     expect(myFizzBuzz(10)).toBe("buzz")
-})
-it('Execute a função myFizzBuzz(num), sendo num um número que não é divisível por 3 ou 5, e verifique se o retorno é o esperado.', () => {
-  expect(myFizzBuzz(7)).toBe(7)
-})
-it('Execute a função myFizzBuzz(num), sendo num um parâmetro que não é um número, e verifique se o retorno é o esperado', () => {
-  expect(myFizzBuzz('dois')).toBeFalsy()
-})
+  })
+  it('Execute a função myFizzBuzz(num), sendo num um número que não é divisível por 3 ou 5, e verifique se o retorno é o esperado.', () => {
+    expect(myFizzBuzz(7)).toBe(7)
+  })
+  it('Execute a função myFizzBuzz(num), sendo num um parâmetro que não é um número, e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('dois')).toBeFalsy()
+  })
 });
