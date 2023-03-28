c059ff708a69453609cbcfa431ba6d12d2b8811e
Jonathan Ramos <jhonyramos46@gmail.com>
2023-03-28 18:48:37 -0300

    exercicio 02

--- 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
+++ 01 - fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
@@ -7,7 +7,15 @@ function myRemove(arr, item) {
   }
   return newArr;
 }
-console.log(myRemove([1, 2, 3, 4]));
-// implemente seus testes aqui
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
 
-module.exports = myRemove;
+
+module.exports = {
+  myRemove, myFizzBuzz
+}
